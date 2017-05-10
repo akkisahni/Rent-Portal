d6c43522cad227a8e8a2cd4b8ba7ce16560f8d64
unknown <asahn3@delasahn375346.sapient.com>
2017-05-10 13:20:47 +0530

    [Form changes initial]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `<!-- main app container -->
-  <div class="jumbotron">
+  <div class="jumbotron backdrop">
       <div class="container">
           <div class="col-sm-8 col-sm-offset-2">              
               <router-outlet></router-outlet>
